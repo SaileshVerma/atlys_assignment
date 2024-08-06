f1bf42fc7db15ed798c2065d4d1388e05991c91b
sailesh <vermasailesh271@gmail.com>
2024-08-06 23:38:28 +0530

    added login screen

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,51 @@
+import EyeIcon from "../assets/icons/EyeIcon";
 import Logo from "../assets/logos/Logo";
 
 function App() {
   return (
-    <div className="">
+    <div className="flex h-screen w-screen flex-col items-center justify-center text-white gap-8">
       <Logo />
+      <div className="flex flex-col gap-5 border rounded-lg items-center justify-center bg-background-black-secondary w-1/3 px-6 py-8">
+        <span className="text-secondary-text-black font-medium text-sm">
+          WELCOME BACK
+        </span>
+        <span className=" font-semibold text-lg">Log into your account</span>
+
+        <div className="flex flex-col self-start gap-2 w-full">
+          <span className="text-grey-1 text-sm font-medium">
+            Email or Username
+          </span>
+          <input
+            className="bg-transparent border border-border-secondary-black rounded p-2 placeholder-style"
+            placeholder="Enter your email or username"
+          />
+        </div>
+        <div className="flex flex-col self-start gap-2 w-full">
+          <div className="flex flex-row justify-between items-center">
+            <span className="text-grey-1 text-sm font-medium">Password</span>
+            <span className="text-grey-1 text-xs font-medium">
+              Forgot password?
+            </span>
+          </div>
+          <div className="relative w-full">
+            <input
+              className="bg-transparent border border-border-secondary-black rounded p-2 w-full placeholder-style"
+              placeholder="Enter your password"
+            />
+            <div className="absolute right-2 top-3">
+              <EyeIcon />
+            </div>
+          </div>
+        </div>
+
+        <button className="w-full p-2 font-medium bg-[#4A96FF]">
+          Login now
+        </button>
+        <div className="flex self-start gap-1 text-sm text-grey-1 font-medium">
+          <span className="text-[#7F8084]">Not registered yet? </span>
+          <button>Register →</button>
+        </div>
+      </div>
     </div>
   );
 }
